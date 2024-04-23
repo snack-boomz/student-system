bf720b0c715bb4ea16bf76f1f0338cd1445af931
Trey Annis <ta08annis@gmail.com>
2024-04-23 08:50:50 -0400

    roster.h staged, building out roster.cpp

--- roster.h
+++ roster.h
@@ -11,7 +11,10 @@ class Student
     public:
         void add(std::string studentID, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
         void remove(std::string studentID);
-        void printAll()
+        void printAll();
+        void printAverageDaysInCourse(std::string studentID);
+        void printInvalidEmails();
+        void printByDegreeProgram(DegreeProgram degreeProgram);
     private:
         std::array<int*, 5> classRosterArray;
         
